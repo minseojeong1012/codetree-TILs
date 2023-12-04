4208b63c3f3c2792f3e0f16b2fc7c791e9678f27
minseojeong <jgoldstone@hufs.ac.kr>
2023-12-04 15:18:36 +0900

    Title: 두 개의 격자 비교하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/두 개의 격자 비교하기/compare-two-grid.py
+++ 231204/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,16 +1,19 @@
-n, m = map(int,input().split())
-arr_1 = [list(map(int,input().split())) for _ in range(n)]
-arr_2 = [list(map(int,input().split())) for _ in range(n)]
-arr_3 = [[0 for _ in range(n)] for _ in range(m)]
+a,b = map(int,input().split())
+n1=[]
+n2=[]
+arr_3 =[[0 for _ in range(b)] for _ in range(a)]
+for _ in range(a):
+    n1.append(list(map(int,input().split())))
 
-for i in range(n):
-    for j in range(m):
-        if arr_1[i][j] == arr_2[i][j]:
-            arr_3[i][j] =0
+for _ in range(a):
+    n2.append(list(map(int,input().split())))
+for i in range(a):
+    for j in range(b):
+        if n1[i][j] == n2[i][j]:
+            arr_3[i][j] = 0
         else:
-            arr_3[i][j] =1
-
-for i in range(n):
-    for j in range(m):
-        print(arr_3[i][j], end =" ")
+            arr_3[i][j] = 1
+for k in range(a):
+    for j in range(b):
+        print(arr_3[k][j],end=' ')
     print()
