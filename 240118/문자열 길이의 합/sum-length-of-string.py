028356091f81db7037a60b0a5e4083b8101b90b6
minseojeong <jgoldstone@hufs.ac.kr>
2024-01-18 16:44:03 +0900

    Title: 문자열 길이의 합, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/문자열 길이의 합/sum-length-of-string.py
+++ 240118/문자열 길이의 합/sum-length-of-string.py
@@ -1,11 +1,9 @@
 # n을 입력받습니다.
 n = int(input())
-
+arr=[]
 # 문자열을 구현하고 입력받습니다.
-string = [
-	input()
-	for _ in range(n)
-]
+for i in range(n):
+	arr.append(input())
 
 # len_all : 전체 문자열의 길이, cnt : 첫번째 문자로 'a'가 나오는 횟수
 len_all = 0
@@ -13,8 +11,8 @@ cnt = 0
 	
 # 전체 문자열의 길이와 첫번째 문자로 'a'가 나오는 횟수를 구합니다.
 for i in range(n):
-	len_all += len(string[i])
-	if string[i][0] == 'a':
+	len_all += len(arr[i])
+	if arr[i][0] == 'a':
 		cnt += 1
 	
 # 문제에서 구하고자 하는 값들을 출력합니다.
