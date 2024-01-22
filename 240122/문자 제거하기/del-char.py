4cb251d609038301a312b532c5378f28a3cecfb3
minseojeong <jgoldstone@hufs.ac.kr>
2024-01-22 17:46:23 +0900

    Title: 문자 제거하기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240122/문자 제거하기/del-char.py
@@ -0,0 +1,15 @@
+s = input()
+arr = list(s)
+
+while len(arr) >1:
+    
+    n = int(input())
+
+    if n >= len(arr):
+        arr.pop(len(arr)-1)
+        s = "".join(arr)
+        print(s)
+    else:
+        arr.pop(n)
+        s = "".join(arr)
+        print(s)
