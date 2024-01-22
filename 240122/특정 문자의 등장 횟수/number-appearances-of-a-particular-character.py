f6c631d3d02223528aa89689a326fb2882d7ec9e
minseojeong <jgoldstone@hufs.ac.kr>
2024-01-22 09:57:16 +0900

    Title: 특정 문자의 등장 횟수, Time: 247ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240122/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -4,9 +4,9 @@ cntt=0
 leng = len(s)
 
 for i in range(leng-1):
-    if 'ee' in s:
+    if 'ee' == s[i:i+2]:
         cnt+=1
-    elif 'eb' in s:
+    elif 'eb' ==s[i:i+2]:
         cntt+=1
 
 print(cnt,cntt)
