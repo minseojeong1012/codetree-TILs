375b6a1fcb2039ebea6ae281f06eed5625949afb
minseojeong <jgoldstone@hufs.ac.kr>
2024-01-29 17:00:50 +0900

    Title: 두 정수에 대한 연산값 2, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240129/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,10 +1,19 @@
-def calc(x,y):
-    a = min(x,y) + 10
-    b = max(x,y) * 2
-    return a,b
+# 변수 선언 및 입력
+a, b = tuple(map(int, input().split()))
 
-a,b = map(int,input().split())
 
-x,y = calc(a,b)
+# 문제에서 설명한 대로 a와 b를 변경합니다.
+def change_number(a, b):
+    if(a > b):
+        a *= 2
+        b += 10
+    else:
+        b *= 2
+        a += 10
 
-print(x,y)
+    return a, b
+
+
+a, b = change_number(a, b)
+
+print(a, b)
