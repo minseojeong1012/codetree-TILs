e5ac75758a6683f10b6e3c63fb3bda94c6dca5ee
minseojeong <jgoldstone@hufs.ac.kr>
2024-02-06 11:37:35 +0900

    Title: 007, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/007/007.py
+++ 240206/007/007.py
@@ -1,11 +1,18 @@
-class Place:
-    def __init__(self,secret_code,meeting_point,time):
+# 클래스 선언
+class Spy:
+    def __init__(self, secret_code, meeting_point, time):
         self.secret_code = secret_code
         self.meeting_point = meeting_point
         self.time = time
 
-place1 = Place('codetree','L',13)
 
-print("secret code :",place1.secret_code)
-print("meeting point :",place1.meeting_point)
-print("time :",place1.time)
+# 변수 선언 및 입력
+s_code, m_point, time = tuple(input().split())
+
+# 객체 생성
+s = Spy(s_code, m_point, int(time))
+
+# 출력
+print(f"secret code : {s.secret_code}")
+print(f"meeting point : {s.meeting_point}")
+print(f"time : {s.time}")
