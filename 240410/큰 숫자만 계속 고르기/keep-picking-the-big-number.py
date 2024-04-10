22fbdf6f19beb8e81b896411ae3a241823acbac8
Minseo Jeong <jgoldstone7@gmail.com>
2024-04-10 22:18:41 +0900

    Title: 큰 숫자만 계속 고르기, Time: 225ms, Memory: 33MB, Status: Passed - CodeTree

--- 240410/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 240410/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -1,27 +1,19 @@
-# ---------------------------------------------
-# N 개의 수를 배열로 관리하기 - O(N*M), 시간 초과
-# ---------------------------------------------
+# -------------------------------------------------
+# N 개의 수를 Priority Queue로 관리하기 - O(M log N)
+# -------------------------------------------------
+import heapq
+
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
-
-
-def find_max_index(arr):
-    """
-    arr "배열"에서 가장 큰 원소가 존재하는 index를 return하는 함수이다. 시간 복잡도는 O(N)이다.
-    """
-    max_value, max_index = arr[0], 0
-    for i in range(1, n):
-        if max_value < arr[i]:
-            max_value, max_index = arr[i], i
-    
-    return max_index
+nums = [-num for num in nums]
+heapq.heapify(nums)
 
 
 for _ in range(m):
-    # 가장 큰 원소 찾기 - O(n)
-    max_index = find_max_index(nums)
+    # 가장 큰 원소 찾고, 삭제까지 수행하기- O(log N)
+    max_value = -heapq.heappop(nums)
     
-    # 해당 원소를 1 감소 시키기 - O(1)
-    nums[max_index] -= 1
+    # 1 줄인 값 추가하기 - O(log N)
+    heapq.heappush(nums, -(max_value - 1))
 
-print(nums[find_max_index(nums)])
+print(-nums[0])  # O(1)
