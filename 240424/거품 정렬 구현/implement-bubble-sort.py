001a7ecf17457552bad760574f4f1898ed881dca
Minseo Jeong <jgoldstone7@gmail.com>
2024-04-24 20:24:46 +0900

    Title: 거품 정렬 구현, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240424/거품 정렬 구현/implement-bubble-sort.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+for i in range(n):
+    for j in range(n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+for i in arr:
+    print(i, end =" ")
