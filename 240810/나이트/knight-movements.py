578ca812b45a32bc7a6b23a8f1f33afb50dda31e
Minseo Jeong <jgoldstone7@gmail.com>
2024-08-10 10:49:23 +0900

    Title: 나이트, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240810/나이트/knight-movements.py
@@ -0,0 +1,81 @@
+import sys
+from collections import deque
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력
+n = int(input())
+r1, c1, r2, c2 = tuple(map(int, input().split()))
+r1, c1, r2, c2 = r1 - 1, c1 - 1, r2 - 1, c2 - 1
+
+# bfs에 필요한 변수들 입니다.
+q = deque()
+visited = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
+step = [                    # step[i][j] : 시작점으로부터 
+    [0 for _ in range(n)]   # (i, j) 지점에 도달하기 위한 
+    for _ in range(n)       # 최단거리를 기록합니다.
+]
+
+ans = INT_MAX
+
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+# 격자를 벗어나지 않으면서, 아직 방문한 적이 없는 곳이라면
+# 지금 이동하는 것이 최단거리임을 보장할 수 있으므로 가야만 합니다. 
+def can_go(x, y):
+    return in_range(x, y) and not visited[x][y]
+
+
+# queue에 새로운 위치를 추가하고
+# 방문 여부를 표시해줍니다.
+# 시작점으로 부터의 최단거리 값도 갱신해줍니다.
+def push(nx, ny, new_step):
+    q.append((nx, ny))
+    visited[nx][ny] = True
+    step[nx][ny] = new_step
+
+
+# bfs를 통해 최소 이동 횟수를 구합니다.
+def find_min():
+    global ans
+    
+    # queue에 남은 것이 없을때까지 반복합니다.
+    while q:
+        # queue에서 가장 먼저 들어온 원소를 뺍니다.
+        x, y = q.popleft()
+
+        dxs = [-2, -2, -1, -1,  1, 1,  2, 2]
+        dys = [-1,  1, -2,  2, -2, 2, -1, 1]
+
+        # queue에서 뺀 원소의 위치를 기준으로 8방향을 확인해봅니다.
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+
+            # 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+            # 새로 queue에 넣어줍니다.
+            if can_go(nx, ny):
+                # 최단 거리는 이전 최단거리에 1이 증가하게 됩니다. 
+                push(nx, ny, step[x][y] + 1)
+    
+    # 도착지에 가는 것이 가능할때만
+    # 답을 갱신해줍니다.
+    if visited[r2][c2]:
+        ans = step[r2][c2]
+
+
+# bfs를 이용해 최소 이동 횟수를 구합니다.
+# 시작점을 queue에 넣고 시작합니다.
+push(r1, c1, 0)
+find_min()
+
+# 출력:
+if ans == INT_MAX:  # 불가능한 경우라면
+    ans = -1        # -1을 답으로 넣어줍니다.
+
+print(ans)
