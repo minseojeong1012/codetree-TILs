305254b6f228759070d40481605ca14f2d6ad0df
Minseo Jeong <jgoldstone7@gmail.com>
2024-08-10 10:24:59 +0900

    Title: 행복한 수열의 개수, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240810/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240810/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,27 +1,43 @@
-def check_happy(sequence, m):
-    count = 1
-    for i in range(1, len(sequence)):
-        if sequence[i] == sequence[i-1]:
-            count += 1
-            if count >= m:
-                return True
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+seq = [0 for _ in range(n)]
+
+
+def is_happy_sequence():
+    # 주어진 seq가 행복한 수열인지 판단하는 함수입니다.
+    consecutive_count, max_ccnt = 1, 1
+    for i in range(1, n):
+        if seq[i - 1] == seq[i]:
+            consecutive_count += 1
         else:
-            count = 1
-    return False
+            consecutive_count = 1
+        
+        max_ccnt = max(max_ccnt, consecutive_count)
+    
+    # 최대로 연속한 회수가 m이상이면 true를 반환합니다. 
+    return max_ccnt >= m
 
-n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
 
-happy_count = 0
+num_happy = 0
 
-# Check rows
-for row in grid:
-    if check_happy(row, m):
-        happy_count += 1
+# 먼저 가로로 행복한 수열의 수를 셉니다.
+for i in range(n):
+    seq = grid[i][:]
 
-# Check columns
-for col in zip(*grid):
-    if check_happy(col, m):
-        happy_count += 1
+    if is_happy_sequence():
+        num_happy += 1
 
-print(happy_count)
+# 세로로 행복한 수열의 수를 셉니다.
+for j in range(n):
+    # 세로로 숫자들을 모아 새로운 수열을 만듭니다.
+    for i in range(n):
+        seq[i] = grid[i][j]
+
+    if is_happy_sequence():
+        num_happy += 1
+
+print(num_happy)
