456c14d02db93d58a098e0f53fff1cc39b5568e8
Minseo Jeong <jgoldstone7@gmail.com>
2024-08-10 10:48:59 +0900

    Title: 최소 경로로 탈출 하기, Time: 250ms, Memory: 27MB, Status: Passed - CodeTree

--- 240810/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240810/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -1,6 +1,5 @@
 import sys
-# recursion의 최대 깊이(N*M)인 10000으로 설정해줍니다.
-sys.setrecursionlimit(10000)
+from collections import deque
 
 INT_MAX = sys.maxsize
 
@@ -12,9 +11,16 @@ a = [
     for _ in range(n)
 ]
 
-# backtracking에 필요한 변수들 입니다.
+# bfs에 필요한 변수들 입니다.
+q = deque()
 visited = [
     [False for _ in range(m)]
+    for _ in range(n)
+]
+# step[i][j] : 시작점으로부터 (i, j) 지점에 도달하기 위한 
+# 최단거리를 기록합니다.
+step = [
+    [0 for _ in range(m)]
     for _ in range(n)
 ]
 
@@ -25,39 +31,49 @@ def in_range(x, y):
 
 
 # 격자를 벗어나지 않으면서, 뱀도 없고, 아직 방문한 적이 없는 곳이라면
-# 이동이 가능합니다.
+# 지금 이동하는 것이 최단거리임을 보장할 수 있으므로 가야만 합니다. 
 def can_go(x, y):
     return in_range(x, y) and a[x][y] and not visited[x][y]
 
 
-# backtracking을 통해 최소 이동 횟수를 구합니다.
-def find_min(x, y, cnt):
-    global ans
+# queue에 새로운 위치를 추가하고
+# 방문 여부를 표시해줍니다.
+# 시작점으로 부터의 최단거리 값도 갱신해줍니다.
+def push(new_x, new_y, new_step):
+    q.append((new_x, new_y))
+    visited[new_x][new_y] = True
+    step[new_x][new_y] = new_step
     
-    if x == n - 1 and y == m - 1:
-        ans = min(ans, cnt)
-        return
+    
+# bfs를 통해 최소 이동 횟수를 구합니다.
+def find_min():
+    global ans
     
     dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
     
-    # 현재 위치를 기준으로 4방향을 확인해봅니다.
-    for dx, dy in zip(dxs, dys):
-        new_x, new_y = x + dx, y + dy
+    # queue에 남은 것이 없을때까지 반복합니다.
+    while q:
+        # queue에서 가장 먼저 들어온 원소를 뺍니다.
+        x, y = q.popleft()    
+    
+        # queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = x + dx, y + dy
         
-        # 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
-        # 더 진행해봅니다.
-        if can_go(new_x, new_y):
-            # 지금까지의 선택이 최단경로 로서 부적합했을 수 있으므로
-            # 퇴각시 visited값을 다시 false로 바꿔 
-            # 다른 방향으로 진행할때도 기회를 주어 모든 가능한 
-            # 경로를 전부 탐색할 수 있도록 합니다. 
-            visited[new_x][new_y] = True
-            find_min(new_x, new_y, cnt + 1)
-            visited[new_x][new_y] = False
-            
-# 현재까지 이동 횟수가 0일때, (0, 0)에서 시작하는
-# 재귀를 호출합니다.
-find_min(0, 0, 0)
+            # 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+            # 새로 queue에 넣어줍니다.
+            if can_go(new_x, new_y):
+                # 최단 거리는 이전 최단거리에 1이 증가하게 됩니다.
+                push(new_x, new_y, step[x][y] + 1)
+    
+    # 우측 하단에 가는 것이 가능할때만 답을 갱신해줍니다.
+    if visited[n - 1][m - 1]:
+        ans = step[n - 1][m - 1]
+
+# bfs를 이용해 최소 이동 횟수를 구합니다.
+# 시작점을 queue에 넣고 시작합니다.
+push(0, 0, 0)
+find_min()
 
 # 불가능한 경우라면 -1을 답으로 넣어줍니다.
 if ans == INT_MAX:
