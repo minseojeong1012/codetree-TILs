ddc605804437bb686032fe11baabf22c9e9474b1
Minseo Jeong <jgoldstone7@gmail.com>
2024-10-02 10:56:26 +0900

    Title: 특정 구간의 합, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/특정 구간의 합/sum-of-specific-intervals.java
+++ 241002/특정 구간의 합/sum-of-specific-intervals.java
@@ -1,38 +1,37 @@
 import java.util.Scanner;
+
 public class Main {
 
-    public static final int MAX_N = 100;
+    public static final int maxNumber = 100;
 
-    public static int[] arr = new int[MAX_N+1];
+    public static int[] arr = new int[maxNumber+1];
 
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        for (int i = 1; i<= n ;i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = sc.nextInt();
         }
 
-
-        for (int i = 0; i < m; i++) {
-            int total = 0;
+        for (int i = 1; i<= m; i++) {
+            int sum = 0;
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int finalValue = addUp(arr, a,b);
-            System.out.println(finalValue);
-            
+            int result = addUp(a,b);
+            System.out.println(result);
         }
     }
-
-    public static int addUp(int[] arr,int a, int b) {
-        int sum = 0;
-        for (int i = a; i<=b; i++) {
-            sum += arr[i];
+    public static int addUp(int a , int b) {
+        int total = 0;
+        for (int i = a; i<=b;i++) {
+            total += arr[i];
         }
-        return sum;
+        return total;
     }
+    
 }
