90957b3e1e515d20c43f3a6d65ab0c40da88bea4
Minseo Jeong <jgoldstone7@gmail.com>
2024-10-06 23:24:47 +0900

    Title: 블럭쌓는 명령2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241006/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_K = 100;
+
+    public static int n,k;
+    public static int[] a = new int[MAX_K+1];
+    public static int[] b = new int[MAX_K+1];
+
+    public static int[] blocks = new int[MAX_N+1];
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int i = 1; i <=k; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+
+        for(int i = 1; i<=k; i++) {
+            for (int j = a[i]; j<= b[i]; j++)
+                blocks[j]++;
+        }
+
+        int max = 0;
+        for(int i = 1; i <=n; i++)
+            if (blocks[i] > max)
+                max = blocks[i];
+
+        System.out.print(max);
+    }
+}
