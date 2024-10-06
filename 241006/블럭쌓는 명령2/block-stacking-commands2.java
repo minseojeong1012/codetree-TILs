e264e227e13568eb56cf2f33a3d7f29eff367bc4
Minseo Jeong <jgoldstone7@gmail.com>
2024-10-06 23:41:02 +0900

    Title: 블럭쌓는 명령2, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 241006/블럭쌓는 명령2/block-stacking-commands2.java
+++ 241006/블럭쌓는 명령2/block-stacking-commands2.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Main {
+
     public static final int MAX_N = 100;
     public static final int MAX_K = 100;
 
-    public static int n,k;
     public static int[] a = new int[MAX_K+1];
     public static int[] b = new int[MAX_K+1];
 
-    public static int[] blocks = new int[MAX_N+1];
+    public static int[] checks = new int[MAX_N+1]; 
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
@@ -16,21 +16,22 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        for(int i = 1; i <=k; i++) {
+        for (int i = 1; i <=k; i++) {
             a[i] = sc.nextInt();
             b[i] = sc.nextInt();
         }
 
-        for(int i = 1; i<=k; i++) {
-            for (int j = a[i]; j<= b[i]; j++)
-                blocks[j]++;
+        for (int i = 1; i<=k; i++) {
+            for (int j = a[i]; j<=b[i]; j++) {
+                checks[j]++;
+            }
         }
-
         int max = 0;
-        for(int i = 1; i <=n; i++)
-            if (blocks[i] > max)
-                max = blocks[i];
-
+        for (int i = 0; i<=n;i++) {
+            if (checks[i]>max) {
+                max = checks[i];
+            }
+        }
         System.out.print(max);
     }
 }
