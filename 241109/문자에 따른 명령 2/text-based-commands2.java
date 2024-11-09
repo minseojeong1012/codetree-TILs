97aef52c14b49999f076b145fc10959879ebcd41
Minseo Jeong <jgoldstone7@gmail.com>
2024-11-09 15:26:58 +0900

    Title: 문자에 따른 명령 2, Time: 197ms, Memory: 11MB, Status: Passed - CodeTree

--- 241109/문자에 따른 명령 2/text-based-commands2.java
+++ 241109/문자에 따른 명령 2/text-based-commands2.java
@@ -2,28 +2,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
 
-        String str = sc.next();
+        Scanner sc = new Scanner(System.in);
 
         int x = 0;
         int y = 0;
 
-        int currentDir = 0;
+        int currentdir = 0;
 
         int[] dx = new int[]{0,1,0,-1};
         int[] dy = new int[]{1,0,-1,0};
 
+        String str = sc.next();
+
         for (int i = 0; i < str.length();i++) {
             char cdir = str.charAt(i);
 
-            if (cdir == 'L') {
-                currentDir = (currentDir - 1 + 4)%4;
-            } else if (cdir == 'R') {
-                currentDir = (currentDir + 1)%4;
+            if (cdir == 'R') {
+                currentdir = (currentdir + 1) % 4;
+            } else if (cdir == 'L') {
+                currentdir = (currentdir - 1 + 4) %4;
             } else {
-                x += dx[currentDir];
-                y += dy[currentDir];
+                x += dx[currentdir];
+                y += dy[currentdir];
             }
         }
         System.out.println(x+" "+y);
