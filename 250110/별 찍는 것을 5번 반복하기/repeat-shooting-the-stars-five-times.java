bddab657ecb30b79fa46cf3a2818bd29fd6d9d3e
Minseo Jeong <jgoldstone7@gmail.com>
2025-01-10 21:19:44 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250110/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void printstars() {
+        for (int i = 0; i<10;i++)
+            System.out.print("*");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int i = 0; i<5;i++)
+            printstars();
+    }
+}
