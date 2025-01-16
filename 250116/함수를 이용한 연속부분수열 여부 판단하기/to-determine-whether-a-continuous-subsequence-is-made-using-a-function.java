70056f914eea9e0a16724bd3d523418fb0553bfe
Minseo Jeong <jgoldstone7@gmail.com>
2025-01-16 10:16:18 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 250116/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 250116/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -26,11 +26,11 @@ public class Main {
         boolean isSubsequence = false;
 
 
-        for (int i = 0; i < n1; i++) {
+        for (int i = 0; i <= n1-n2; i++) {
 
             boolean isMatch = true;
             
-            for (int j = 0; j <= n1-n2; j++) {
+            for (int j = 0; j < n2; j++) {
                 if (a[i+j] != b[j]) {
                     isMatch = false;
                     break;
