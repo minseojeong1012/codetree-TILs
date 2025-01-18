95ac021a8105a3cb2bda2663e5ad3b6068471ea2
Minseo Jeong <jgoldstone7@gmail.com>
2025-01-18 17:24:22 +0900

    Title: 중앙값 계산 2, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250118/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (i%2==0) {
+                Arrays.sort(arr, 0, i+1);
+                System.out.print(arr[i/2]+ " ");
+            }
+        }
+
+    }
+}
